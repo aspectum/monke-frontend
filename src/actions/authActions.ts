4491a328fb3e8b69f2ffdf2ba112d1b8f4513c0a
Pedro Garcia <drogpe@gmail.com>
2021-03-22 17:25:27 -0300

    Set up axios interceptor to handle headers and api url

--- src/actions/authActions.ts
+++ src/actions/authActions.ts
@@ -1,39 +1,14 @@
-import axios from 'axios';
-import { AppDispatch, AppGetState } from '../store';
+import axios from '../helpers/apiClient';
+import { AppDispatch } from '../store';
 
-import apiURL from '../helpers/apiURL';
 import { AUTH_SUCCESS, AUTH_FAIL, AUTH_CHECKING, AUTH_VERIFIED } from './authActionTypes';
 
-interface AxiosConfig {
-    headers: {
-        'Content-Type': string;
-        Authorization?: string;
-    };
-}
-
-export const sendWithToken = (getState: AppGetState) => {
-    // Get token from state
-    const { token } = getState().authReducer;
-
-    const config: AxiosConfig = {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    };
-
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-};
-
 // VERIFY IF USER IS AUTHENTICATED
-export const verifyUser = () => (dispatch: AppDispatch, getState: AppGetState) => {
+export const verifyUser = () => (dispatch: AppDispatch) => {
     dispatch({ type: AUTH_CHECKING });
 
     axios
-        .get(apiURL('/auth/'), sendWithToken(getState))
+        .get('/auth/')
         .then((res) => dispatch({ type: AUTH_VERIFIED, payload: res.data }))
         .catch((err) => {
             dispatch({ type: AUTH_FAIL });
@@ -43,18 +18,11 @@ export const verifyUser = () => (dispatch: AppDispatch, getState: AppGetState) =
 
 // LOGIN
 export const login = (email: string, password: string) => (dispatch: AppDispatch) => {
-    // Headers
-    const config: AxiosConfig = {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    };
-
     // Request Body
     const body = JSON.stringify({ email, password });
 
     axios
-        .post(apiURL('/auth/login'), body, config)
+        .post('/auth/login', body)
         .then((res) => dispatch({ type: AUTH_SUCCESS, payload: res.data }))
         .catch((err) => {
             dispatch({ type: AUTH_FAIL });
--- src/helpers/apiURL.ts
+++
@@ -1,3 +0,0 @@
-const API_URL = 'http://localhost:5000';
-
-export default (endpoint: string) => API_URL + endpoint;
