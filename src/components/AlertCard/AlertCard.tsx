567b578fc5644584d88ce31d1619aad8b9869125
Pedro Garcia <drogpe@gmail.com>
2021-03-23 16:33:21 -0300

    Created AlertCard component

---
+++ src/components/AlertCard/AlertCard.tsx
@@ -0,0 +1,28 @@
+import React, { ReactElement } from 'react';
+import './AlertCard.scss';
+
+interface Props {
+    imageUrl: string;
+    title: string;
+    targetPrice: number;
+    onClick: () => void;
+    active: boolean;
+}
+
+function AlertCard({ imageUrl, title, targetPrice, onClick, active }: Props): ReactElement {
+    return (
+        <div className={`alert-card ${active ? 'active' : ''}`} onClick={onClick}>
+            <div className="alert-image">
+                <img src={imageUrl} alt="" />
+            </div>
+            <div className="alert-desc">
+                <span className="alert-title">{title}</span>
+                <span>
+                    Target price: <span className="alert-target-price">$ {targetPrice}</span>
+                </span>
+            </div>
+        </div>
+    );
+}
+
+export default AlertCard;
