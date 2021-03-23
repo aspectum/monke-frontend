b852ff471cffc80156bd9bdf57b709a3eb7a4ac1
Pedro Garcia <drogpe@gmail.com>
2021-03-23 17:46:12 -0300

    Trying to customize graph style

--- src/components/PriceGraph/PriceGraph.tsx
+++ src/components/PriceGraph/PriceGraph.tsx
@@ -1,6 +1,10 @@
-import React, { ReactElement } from 'react';
 import { ResponsiveLine } from '@nivo/line';
+import React, { ReactElement } from 'react';
 import { PriceData } from '../../actions/alertActionTypes';
+import dateFormatter from '../../helpers/dateFormatter';
+
+// MORE FORMATTING OPTIONS
+// https://nivo.rocks/storybook/?path=/story/line--formatting-axis-values
 
 interface Props {
     priceHistory: PriceData[];
@@ -24,40 +28,69 @@ function parsePriceHistory(priceHistory: PriceData[]) {
     ];
 }
 
+const nivoTheme = {
+    textColor: '#dddddd',
+};
+
+const tooltipFormatter = ({ point }: any) => {
+    return (
+        <div
+            style={{
+                backgroundColor: 'rgba(21, 23, 35, 0.78)',
+                display: 'flex',
+                flexDirection: 'column',
+                padding: '5px 10px',
+                borderRadius: '5px',
+                color: '#eee',
+            }}
+        >
+            <span>
+                <strong>Date:</strong> {dateFormatter(point.data.x)}
+            </span>
+            <span>
+                <strong>Price:</strong> {`$ ${point.data.y.toFixed(2)}`}
+            </span>
+        </div>
+    );
+};
+
 function PriceGraph({ priceHistory }: Props): ReactElement {
     const nivoData = parsePriceHistory(priceHistory) as any;
 
     return (
         <ResponsiveLine
             data={nivoData}
-            colors={{ scheme: 'nivo' }}
+            colors="#1439b4"
+            theme={nivoTheme}
             margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
             xScale={{
                 type: 'time',
                 // format: '%Y-%m-%d',
                 format: 'native',
-                precision: 'day',
+                // precision: 'auto',
             }}
-            xFormat="time:%Y-%m-%d" // tooltip
+            // xFormat="time:%Y-%m-%d" // tooltip
+            tooltip={tooltipFormatter}
+            // xFormat={dateFormatter} // tooltip
             yScale={{
                 type: 'linear',
-                min: 'auto',
+                min: 0,
                 max: 'auto',
                 stacked: true,
                 reverse: false,
             }}
-            yFormat=" >-$.2f"
+            // yFormat=" >-$.2f"
             axisTop={null}
             axisRight={null}
             axisBottom={{
                 orient: 'bottom',
                 format: '%b %d',
-                tickValues: 'every day',
+                // tickValues: 'every day', // automatic scale this way
                 tickSize: 5,
                 tickPadding: 5,
-                tickRotation: 0,
-                legend: 'transportation',
-                legendOffset: 36,
+                tickRotation: -30,
+                legend: 'Date',
+                legendOffset: 40,
                 legendPosition: 'middle',
             }}
             axisLeft={{
@@ -65,7 +98,7 @@ function PriceGraph({ priceHistory }: Props): ReactElement {
                 tickSize: 5,
                 tickPadding: 5,
                 tickRotation: 0,
-                legend: 'count',
+                legend: 'Price',
                 legendOffset: -40,
                 legendPosition: 'middle',
             }}
@@ -75,32 +108,32 @@ function PriceGraph({ priceHistory }: Props): ReactElement {
             pointBorderColor={{ from: 'serieColor' }}
             pointLabelYOffset={-12}
             useMesh
-            legends={[
-                {
-                    anchor: 'bottom-right',
-                    direction: 'column',
-                    justify: false,
-                    translateX: 100,
-                    translateY: 0,
-                    itemsSpacing: 0,
-                    itemDirection: 'left-to-right',
-                    itemWidth: 80,
-                    itemHeight: 20,
-                    itemOpacity: 0.75,
-                    symbolSize: 12,
-                    symbolShape: 'circle',
-                    symbolBorderColor: 'rgba(0, 0, 0, .5)',
-                    effects: [
-                        {
-                            on: 'hover',
-                            style: {
-                                itemBackground: 'rgba(0, 0, 0, .03)',
-                                itemOpacity: 1,
-                            },
-                        },
-                    ],
-                },
-            ]}
+            // legends={[
+            //     {
+            //         anchor: 'bottom-right',
+            //         direction: 'column',
+            //         justify: false,
+            //         translateX: 100,
+            //         translateY: 0,
+            //         itemsSpacing: 0,
+            //         itemDirection: 'left-to-right',
+            //         itemWidth: 80,
+            //         itemHeight: 20,
+            //         itemOpacity: 0.75,
+            //         symbolSize: 12,
+            //         symbolShape: 'circle',
+            //         symbolBorderColor: 'rgba(0, 0, 0, .5)',
+            //         effects: [
+            //             {
+            //                 on: 'hover',
+            //                 style: {
+            //                     itemBackground: 'rgba(0, 0, 0, .03)',
+            //                     itemOpacity: 1,
+            //                 },
+            //             },
+            //         ],
+            //     },
+            // ]}
         />
     );
 }
