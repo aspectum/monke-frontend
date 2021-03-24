f4da2bb4f213fe6a92b4a85ef0176436647ea02f
Pedro Garcia <drogpe@gmail.com>
2021-03-24 12:06:37 -0300

    Adding modal action to NewAlertButton onClick

--- src/components/NewAlertButton/NewAlertButton.tsx
+++ src/components/NewAlertButton/NewAlertButton.tsx
@@ -1,9 +1,16 @@
 import React, { ReactElement } from 'react';
+import { useDispatch } from 'react-redux';
+import { showNewAlertModal } from '../../actions/modalActions';
 import './NewAlertButton.scss';
 
 function NewAlertButton(): ReactElement {
+    const dispatch = useDispatch();
+    const onClick = () => {
+        dispatch(showNewAlertModal());
+    };
+
     return (
-        <button className="btn-new-alert" type="button">
+        <button className="btn-new-alert" type="button" onClick={onClick}>
             + Add new alert
         </button>
     );
