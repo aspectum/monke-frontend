bd11c333d325f47dceb6f48af92a241b967cebba
Pedro Garcia <drogpe@gmail.com>
2021-03-25 10:53:15 -0300

    Trying to use CustomForm in place of regular html form

--- src/components/NewAlertForm/NewAlertForm.tsx
+++ src/components/NewAlertForm/NewAlertForm.tsx
@@ -1,6 +1,9 @@
 import React, { ReactElement, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { createNewAlert } from '../../actions/alertActions';
+// import ButtonSubmit from '../CustomForm/ButtonSubmit';
+// import CustomForm from '../CustomForm/CustomForm';
+// import TextInput from '../CustomForm/TextInput';
 import './NewAlertForm.scss';
 
 interface State {
@@ -22,13 +25,23 @@ function NewAlertForm(): ReactElement {
         e.preventDefault();
         dispatch(createNewAlert(state['na-url'], +state['na-price']));
     };
+    // const onSubmit = (fields: any) => {
+    //     dispatch(createNewAlert(fields['na-url'], +fields['na-price']));
+    // };
 
     // Updates the state with the inputs
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setState({ [e.target.name]: e.target.value } as any);
     };
 
+    console.log('rendering');
+
     return (
+        // <CustomForm className="form-new-alert" onSubmit={onSubmit}>
+        //     <TextInput id="na-url" label="Amazon URL:" />
+        //     <TextInput id="na-price" label="Target Price:" />
+        //     <ButtonSubmit className="btn-add-alert" text="Add Alert" />
+        // </CustomForm>
         <form onSubmit={onSubmit}>
             <div className="form-new-alert">
                 <label htmlFor="na-url" id="na-url-label">
