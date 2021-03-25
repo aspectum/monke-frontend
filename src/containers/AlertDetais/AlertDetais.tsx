108ab1d6a8ddcfca63fb55d85d3d0df80c4e29bb
Pedro Garcia <drogpe@gmail.com>
2021-03-25 11:55:56 -0300

    Changed AlertDetails to functional component

--- src/containers/AlertDetais/AlertDetais.tsx
+++ src/containers/AlertDetais/AlertDetais.tsx
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { AlertDetailedData } from '../../actions/alertActionTypes';
+import React, { ReactElement } from 'react';
+import { useSelector } from 'react-redux';
 import ButtonSubmit from '../../components/CustomForm/ButtonSubmit';
 import CustomForm from '../../components/CustomForm/CustomForm';
 import TextInput from '../../components/CustomForm/TextInput';
@@ -9,163 +8,63 @@ import dateFormatter from '../../helpers/dateFormatter';
 import { RootState } from '../../store';
 import './AlertDetais.scss';
 
-interface Props {
-    alert: AlertDetailedData;
-    isLoading: boolean;
-    isError: boolean;
-}
+function AlertDetais(): ReactElement {
+    const state = useSelector((store: RootState) => ({
+        alert: store.alertReducer.selectedAlert,
+        isLoading: store.alertReducer.selectedAlertLoading,
+        isError: store.alertReducer.selectedAlertError,
+    }));
 
-// Inputs
-interface State {
-    title: string;
-    price: string;
-}
-
-class AlertDetais extends Component<Props, State> {
-    state = {
-        title: '',
-        price: '',
-    };
-
-    // componentDidMount() {
-    //     if (this.props.alert) {
-    //         this.setState({
-    //             title: this.props.alert.product.title,
-    //             price: this.props.alert.targetPrice.toString(),
-    //         });
-    //     }
-    // }
-
-    componentDidUpdate(prevProps: Props) {
-        if (this.props.alert !== prevProps.alert) {
-            if (this.props.alert) {
-                // eslint-disable-next-line react/no-did-update-set-state
-                this.setState({
-                    title: this.props.alert.product.title,
-                    price: this.props.alert.targetPrice.toString(),
-                });
-            }
-        }
-    }
-
-    onSubmit = () => {
-        console.log('absolutely nothing');
-    };
-
-    // Updates the state with the inputs
-    onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        this.setState({ [e.target.name]: e.target.value } as any);
-    };
-
-    render() {
-        const { alert, isLoading, isError } = this.props;
-        if (isLoading) {
-            return (
-                <div className="alert-details">
-                    <div style={{ margin: 'auto' }}>Loading</div>
-                </div>
-            );
-        }
-        if (isError) {
-            return (
-                <div className="alert-details">
-                    <div style={{ margin: 'auto' }}>Error</div>
-                </div>
-            );
-        }
-        if (!alert) {
-            return (
-                <div className="alert-details">
-                    <div style={{ margin: 'auto' }}>No alert selected</div>
-                </div>
-            );
-        }
-        const lowestPrice = alert.product.priceHistory[0];
+    // RENDERING
+    const { alert, isLoading, isError } = state;
+    if (isLoading) {
         return (
             <div className="alert-details">
-                <div className="price-history-graph">
-                    <PriceGraph priceHistory={alert.product.priceHistory} />
-                </div>
-                <div className="alert-config">
-                    <div className="lowest-price">
-                        {/* // TODO: Put actual lowest price and parse correctly values */}
-                        <h2>
-                            Lowest Price: $ {lowestPrice.price} in{' '}
-                            {dateFormatter(new Date(+lowestPrice.date))}
-                        </h2>
-                    </div>
-                    <CustomForm onSubmit={(fields) => console.log(fields)}>
-                        <TextInput id="inpt" label="Input for test" value="valor" />
-                        <TextInput id="inpt2" label="Input2 for test" />
-                        <ButtonSubmit text="Button" />
-                    </CustomForm>
-                    <form onSubmit={this.onSubmit}>
-                        <div className="form-edit-alert">
-                            <label htmlFor="ea-title" id="ea-title-label">
-                                Title:{' '}
-                            </label>
-                            <input
-                                type="text"
-                                id="ea-title"
-                                name="title"
-                                value={this.state.title}
-                                onChange={this.onChange}
-                            />
-                            <label htmlFor="ea-price" id="ea-price-label">
-                                Target price:{' '}
-                            </label>
-                            <input
-                                type="text"
-                                id="ea-price"
-                                name="price"
-                                value={this.state.price}
-                                onChange={this.onChange}
-                            />
-                            <button type="submit">Confirm</button>
-                            <button type="button">Delete</button>
-                        </div>
-                    </form>
-                    {/* <form>
-                        <div className="alert-form-table">
-                            <div className="row">
-                                <div className="col labels">
-                                    <div className="row title">
-                                        <label htmlFor="title">Title: </label>
-                                    </div>
-                                    <div className="row price">
-                                        <label htmlFor="price">Target price: </label>
-                                    </div>
-                                </div>
-                                <div className="col inputs">
-                                    <div className="row title">
-                                        <input type="text" id="title" value={alert.product.title} />
-                                    </div>
-                                    <div className="row price">
-                                        <input
-                                            type="text"
-                                            id="price"
-                                            value={`$ ${alert.targetPrice}`}
-                                        />
-                                    </div>
-                                </div>
-                                <div className="col button">
-                                    <div className="submit">
-                                        <button type="submit">Confirm</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </form> */}
-                </div>
+                <div style={{ margin: 'auto' }}>Loading</div>
             </div>
         );
     }
+    if (isError) {
+        return (
+            <div className="alert-details">
+                <div style={{ margin: 'auto' }}>Error</div>
+            </div>
+        );
+    }
+    if (!alert) {
+        return (
+            <div className="alert-details">
+                <div style={{ margin: 'auto' }}>No alert selected</div>
+            </div>
+        );
+    }
+    const lowestPrice = alert.product.priceHistory[0];
+    return (
+        <div className="alert-details">
+            <div className="price-history-graph">
+                <PriceGraph priceHistory={alert.product.priceHistory} />
+            </div>
+            <div className="alert-config">
+                <div className="lowest-price">
+                    {/* // TODO: Put actual lowest price and parse correctly values */}
+                    <h2>
+                        Lowest Price: $ {lowestPrice.price} in{' '}
+                        {dateFormatter(new Date(+lowestPrice.date))}
+                    </h2>
+                </div>
+                <CustomForm className="form-edit-alert" onSubmit={(fields) => console.log(fields)}>
+                    <TextInput id="ea-title" label="Title: " value={alert.product.title} />
+                    <TextInput
+                        id="ea-price"
+                        label="Target price: "
+                        value={alert.targetPrice.toString()}
+                    />
+                    <ButtonSubmit className="btn-ea btn-edit-alert" text="Edit" />
+                    <ButtonSubmit className="btn-ea btn-delete-alert" text="Delete" />
+                </CustomForm>
+            </div>
+        </div>
+    );
 }
 
-const mapStateToProps = (state: RootState) => ({
-    alert: state.alertReducer.selectedAlert,
-    isLoading: state.alertReducer.selectedAlertLoading,
-    isError: state.alertReducer.selectedAlertError,
-});
-
-export default connect(mapStateToProps, null)(AlertDetais);
+export default AlertDetais;
