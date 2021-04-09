8d68d12baafbbc7d1b0845b38dd96a1b0293ce95
Pedro Garcia <drogpe@gmail.com>
2021-04-09 13:51:15 -0300

    Migrated AlertCard

--- src/components/AlertCard/AlertCard.tsx
+++ src/components/AlertCard/AlertCard.tsx
@@ -1,5 +1,54 @@
 import React, { ReactElement } from 'react';
-import './AlertCard.scss';
+import styled from 'styled-components';
+import { alertCardPriceColor, colorPrimary0 } from '../../styles/colors';
+import { numberCardsShowing } from '../../styles/global';
+
+const Card = styled.div`
+    display: flex;
+    height: calc(100% / ${numberCardsShowing});
+    padding: 10px 5px;
+    margin-left: 6px; // Accounting for scrollbar width
+    border-right: 1px solid #fff;
+
+    ${({ active }) => {
+        if (active) {
+            return `
+                background-color: ${colorPrimary0};
+                border: 1px solid #fff;
+                border-right: none;
+            `;
+        }
+        return '';
+    }}
+`;
+
+const AlertImage = styled.div`
+    height: 100%;
+    width: 25%;
+    flex-grow: 1; // grow because the description will have fixed width
+
+    // Use the available space, but don't go over it
+    img {
+        height: 100%;
+        width: 100%;
+        object-fit: contain;
+        margin: auto;
+    }
+`;
+
+const AlertDescription = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    height: 100%;
+    width: 120px; // TODO: do this in em
+    padding: 0 15px;
+`;
+
+const AlertTargetPrice = styled.span`
+    color: ${alertCardPriceColor};
+    font-weight: 900;
+`;
 
 interface Props {
     imageUrl: string;
@@ -9,20 +58,16 @@ interface Props {
     active: boolean;
 }
 
-function AlertCard({ imageUrl, title, targetPrice, onClick, active }: Props): ReactElement {
-    return (
-        <div className={`alert-card ${active ? 'active' : ''}`} onClick={onClick}>
-            <div className="alert-image">
-                <img src={imageUrl} alt="" />
-            </div>
-            <div className="alert-desc">
-                <span className="alert-title">{title}</span>
-                <span>
-                    Target price: <span className="alert-target-price">{targetPrice}</span>
-                </span>
-            </div>
-        </div>
-    );
-}
-
-export default AlertCard;
+export default ({ imageUrl, title, targetPrice, onClick, active }: Props): ReactElement => (
+    <Card active={active} onClick={onClick}>
+        <AlertImage>
+            <img src={imageUrl} alt="" />
+        </AlertImage>
+        <AlertDescription>
+            <span className="alert-title">{title}</span>
+            <span>
+                Target price: <AlertTargetPrice>{targetPrice}</AlertTargetPrice>
+            </span>
+        </AlertDescription>
+    </Card>
+);
