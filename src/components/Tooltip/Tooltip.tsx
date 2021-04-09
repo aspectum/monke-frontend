e27f5a8f034babc95b3ca42d5942c401b1f92c79
Pedro Garcia <drogpe@gmail.com>
2021-04-09 15:02:06 -0300

    Migrated Tooltip

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,6 +1,44 @@
-/* eslint-disable react/require-default-props */
 import React, { ReactElement } from 'react';
-import './Tooltip.scss';
+import styled from 'styled-components';
+import { tooltipBackgroundColor, tooltipTextColor } from '../../styles/colors';
+
+interface TooltipComponentProps {
+    visible: boolean;
+}
+
+const TooltipComponent = styled.div<TooltipComponentProps>`
+    visibility: ${({ visible }) => (visible ? 'visible' : 'hidden')};
+    text-align: center;
+    width: 160px;
+    height: fit-content;
+    background-color: ${tooltipBackgroundColor};
+    color: ${tooltipTextColor};
+    padding: 5px 0;
+    border-radius: 6px;
+
+    /* Position the tooltip text */
+    position: relative;
+    z-index: 1;
+    top: 50%; // aligning with input
+    transform: translate(0, -50%);
+    margin-left: 5px; // space for arrow
+
+    /* Fade in tooltip */
+    opacity: ${({ visible }) => (visible ? 1 : 0)};
+    transition: opacity 0.3s;
+
+    /* Tooltip arrow */
+    &::after {
+        content: '';
+        position: absolute;
+        right: 100%;
+        top: 50%;
+        margin-top: -5px;
+        border-width: 5px;
+        border-style: solid;
+        border-color: transparent ${tooltipBackgroundColor} transparent transparent;
+    }
+`;
 
 interface Props {
     text: string;
@@ -8,11 +46,11 @@ interface Props {
     visible?: boolean;
 }
 
-function Tooltip({ text, id, visible }: Props): ReactElement {
+function Tooltip({ text, id, visible = false }: Props): ReactElement {
     return (
-        <div className={`tooltip ${visible ? 'visible' : ''}`} id={id}>
+        <TooltipComponent visible={visible} id={id}>
             {text}
-        </div>
+        </TooltipComponent>
     );
 }
 
