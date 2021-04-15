369b59a082ab5dc1cc0dfc438a92ba407964cb22
Pedro Garcia <drogpe@gmail.com>
2021-04-15 15:55:58 -0300

    Created component to implement CSS grid

---
+++ src/components/Grid/GridItem.ts
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export type GridItemProps =
+    | { GridRow: React.CSSProperties['gridRow'] }
+    | { GridCol: React.CSSProperties['gridColumn'] };
+
+export default styled.div<GridItemProps>`
+    display: flex;
+    ${({ GridRow }: any) => (GridRow ? `grid-row: ${GridRow};` : '')}
+    ${({ GridCol }: any) => (GridCol ? `grid-column: ${GridCol};` : '')}
+`;
---
+++ src/components/Grid/GridWrapper.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+
+interface GridWrapperBasics {
+    height?: React.CSSProperties['height'];
+    width?: React.CSSProperties['width'];
+}
+
+interface GridWrapperWithRows extends GridWrapperBasics {
+    TemplateRows: React.CSSProperties['gridTemplateRows'];
+}
+
+interface GridWrapperWithCols extends GridWrapperBasics {
+    TemplateCols: React.CSSProperties['gridTemplateColumns'];
+}
+
+export type GridWrapperProps = GridWrapperWithRows | GridWrapperWithCols;
+
+export default styled.div<GridWrapperProps>`
+    display: grid;
+    ${({ TemplateRows }: any) => (TemplateRows ? `grid-template-rows: ${TemplateRows};` : '')}
+    ${({ TemplateCols }: any) => (TemplateCols ? `grid-template-columns: ${TemplateCols};` : '')}
+    ${({ height }) => (height ? `height: ${height};` : '')}
+    ${({ width }) => (width ? `width: ${width};` : '')}
+`;
